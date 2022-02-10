3cbca02fe80d238fce6bba466fa52ca50cdcb2c2
Pratik <71760533+pm-bhatt@users.noreply.github.com>
2022-02-10 12:38:27 +0530

    Okta Script to remove the de-activated users
    Okta Script to remove the de-activated users

---
+++ Remove De-activated users.ps1
@@ -0,0 +1,20 @@
+#script version 1.0
+#Okta API from https://github.com/gabrielsroka/OktaAPI.psm1
+
+#Connecting to Okta API 
+Connect-Okta "00xEreN7P4z6HyMrLRIUy5jWUa_UKPcVBhAjpo9slH" "https://dev-78130576.okta.com/"
+#Getting De-Activated user's llist in Objects
+$page = Get-OktaUsers -filter 'status eq "DEPROVISIONED"'
+$users = $page.objects
+#condition in-case there are no de-activated users, print that no deactivated  users are there.
+if ($null -eq $page.objects) { 
+    Write-Output "No De-activated Users at  $(Get-Date)"
+}
+#if de-activated users are there, then action of deleting to be performed.
+else {
+    foreach ($user in $users) {
+        Remove-OktaUser $user.id
+        Write-Output "Deleted -> $($user.profile.firstName) $($user.profile.lastName)"                   
+     }
+}
+#code ends
