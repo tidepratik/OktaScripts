cd6c61686b9b67703cc4d1d14de6485c3da402cf
Pratik <71760533+pm-bhatt@users.noreply.github.com>
2022-02-10 12:46:41 +0530

    Removed the specific okta references
    Removed the specific okta references

--- Remove De-activated users.ps1
+++ Remove De-activated users.ps1
@@ -2,7 +2,7 @@
 #Okta API from https://github.com/gabrielsroka/OktaAPI.psm1
 
 #Connecting to Okta API 
-Connect-Okta "00xEreN7P4z6HyMrLRIUy5jWUa_UKPcVBhAjpo9slH" "https://dev-78130576.okta.com/"
+Connect-Okta "OKTA API TOKEN" "https://YOUR OKTA TENANT.okta.com/"
 #Getting De-Activated user's llist in Objects
 $page = Get-OktaUsers -filter 'status eq "DEPROVISIONED"'
 $users = $page.objects
